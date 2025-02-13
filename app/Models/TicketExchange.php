045278ae500f5ca1eb8daf1a0f85202d9c75901c
chauve-dev <jeantetjoey.pro@gmail.com>
2025-02-13 19:08:01 +0100

    Feat - Ticket - add ticketExchange to user privacy test

--- app/Models/TicketExchange.php
+++ app/Models/TicketExchange.php
@@ -12,6 +12,18 @@ class TicketExchange extends Model
     /** @use HasFactory<TicketExchangeFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'user_id',
+        'exchange',
+        'type'
+    ];
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
     public function ticket(): BelongsTo
     {
         return $this->belongsTo(Ticket::class);
--- tests/Unit/UserPrivacyTest.php
+++ tests/Unit/UserPrivacyTest.php
@@ -2,19 +2,20 @@
 
 use App\Models\Ticket;
 use App\Models\User;
+use App\TicketExchangeType;
 
 uses(Tests\TestCase::class);
 
 test('if ticket reference to user is redirected to anonymous if user is deleted', function () {
     // the anonymous user id is -1
-    $anonyme = User::find(-1);
+    $anonymous = User::find(-1);
 
     $user1 = User::factory()->create();
     $user2 = User::factory()->create();
 
     $ticket = Ticket::factory()->create([
-        'assignee' => $user1->id,
-        'reporter' => $user2->id,
+        'assignee' => $user1,
+        'reporter' => $user2,
     ]);
 
     expect($ticket->assignee)->toBe($user1->id)
@@ -23,12 +24,48 @@ test('if ticket reference to user is redirected to anonymous if user is deleted'
     $user1->delete();
     $ticket->refresh();
 
-    expect($ticket->assignee)->toBe($anonyme->id)
+    expect($ticket->assignee)->toBe($anonymous->id)
         ->and($ticket->reporter)->toBe($user2->id);
 
     $user2->delete();
     $ticket->refresh();
 
-    expect($ticket->assignee)->toBe($anonyme->id)
-        ->and($ticket->reporter)->toBe($anonyme->id);
+    expect($ticket->assignee)->toBe($anonymous->id)
+        ->and($ticket->reporter)->toBe($anonymous->id);
+});
+
+test('if ticket exchanges get correct user if deleted', function () {
+    // the anonymous user id is -1
+    $anonymous = User::find(-1);
+
+    $user = User::factory()->create()->refresh();
+    $commenter = User::factory()->create()->refresh();
+
+    expect($user->id)->not->toBe(-1)
+        ->and($commenter->id)->not->toBe(-1);
+
+    $ticket = Ticket::factory()->create(['reporter' => $user->id]);
+
+    $exchange_commenter = $ticket->exchanges()->create([
+        'user_id' => $commenter->id,
+        'type' => TicketExchangeType::COMMENT,
+        'exchange' => 'I have a problem with this ticket.',
+    ]);
+
+    $exchange_user = $ticket->exchanges()->create([
+        'user_id' => $user->id,
+        'type' => TicketExchangeType::COMMENT,
+        'exchange' => 'What is the problem?.',
+    ]);
+
+    expect($exchange_commenter->user->id)->toBe($commenter->id)
+        ->and($exchange_user->user->id)->toBe($user->id);
+
+    $commenter->delete();
+
+    $exchange_commenter->refresh();
+
+    // the exchange is correctly wiped of original user info on deletion
+    expect($exchange_commenter->user->id)->toBe($anonymous->id)
+        ->and($exchange_user->user->id)->toBe($user->id);
 });
